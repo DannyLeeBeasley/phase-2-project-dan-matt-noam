6e4b0407a435f374949612151972185bb9fd729c
Dan Beasley <dannyleebeasley@gmail.com>
2021-07-13 11:34:58 -0500

    switching drivers

---
+++ src/components/SmoothiePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import IngredientList from './IngredientList';
+import YourSmoothie from './YourSmoothie';
+
+function SmoothiePage() {
+    return (
+        <div>
+            <IngredientList/>
+            <YourSmoothie/>
+        </div>
+    );
+}
+
+export default SmoothiePage;
